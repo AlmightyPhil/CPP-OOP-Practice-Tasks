f9e821a60987293bbbc732f105a939b8207d811e
Filip Jochic <filip.jochic@gmail.com>
2024-01-31 20:02:24 +0200

    Adding C++ Coding Task #1 310124
    simple Task for Library Management System

---
+++ LibraryManagementSystem/Book.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "Book.h"
+
+using namespace std;
+
+Book::Book(string t, string a, string isbn) : title(t), autor(a), ISBN(isbn)
+{
+	
+}
+
+// display() prints the book fields
+void Book::display()
+{
+	cout << "Book title " << this->title
+		<< " written by " << this->autor
+		<< " with ISBN " << this->ISBN << endl;
+}
+
