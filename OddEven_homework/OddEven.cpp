5c9fad41e89d59b1055675ec0baa2b9f917cc02c
Filip Jochic <filip.jochic@gmail.com>
2024-02-15 11:41:18 +0200

    OddEven Homework added

---
+++ OddEven_homework/OddEven.cpp
@@ -0,0 +1,58 @@
+// OddEven.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+bool isEven(int);
+bool isOdd(int);
+
+int main()
+{
+
+    int arr[100];
+    int temp = 0;
+
+    cout << "Input int numbers, divided by spacebars or one after another with enter: " << endl;
+
+    while (cin >> arr[temp])
+    {
+        temp++;
+    }
+
+    for (int i = 0; i < temp; i++)
+    {
+        if (isEven(arr[i]))
+        {
+            cout << arr[i] << " is even! " << endl;
+        }
+        else
+        {
+            cout << arr[i] << " is odd! " << endl;
+        }
+        
+    }
+
+    return 0;
+};
+
+bool isEven(int n)
+{
+    return n % 2 == 0;
+}
+
+bool isOdd(int n)
+{
+    return n % 2 == 1;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
